d3dfb19a2242bd42adc1038de858fcb13f7b5c83
Gorky Muñoz <gorkymunoz@hotmail.com>
2019-08-15 12:05:40 -0600

    bottom navigation con colores y navegacion completa

--- Mood/app/src/main/java/com/example/moodapp/Navigation/PrincipalActivity.kt
+++ Mood/app/src/main/java/com/example/moodapp/Navigation/PrincipalActivity.kt
@@ -2,17 +2,28 @@ package com.example.moodapp.Navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.moodapp.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 
 class PrincipalActivity : AppCompatActivity() {
 
+    private lateinit var principalBottonNavigation: BottomNavigationView
     private lateinit var auth:FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
+
+        principalBottonNavigation = findViewById(R.id.principal_bottom_navigation)
+        val navController = findNavController(R.id.principal_host_fragment)
+        principalBottonNavigation.setupWithNavController(navController)
+
         auth = FirebaseAuth.getInstance()
-        auth.signOut()
+        //auth.signOut()
     }
+
+
 }
