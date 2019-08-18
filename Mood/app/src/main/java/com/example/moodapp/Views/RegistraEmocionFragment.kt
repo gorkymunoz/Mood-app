8c237eb11bf26aa611eb232edb14608cbdc61a43
Gorky Muñoz <gorkymunoz@hotmail.com>
2019-08-18 14:53:09 -0600

    vista registra emocion

--- Mood/app/src/main/java/com/example/moodapp/Views/RegistraEmocionFragment.kt
+++ Mood/app/src/main/java/com/example/moodapp/Views/RegistraEmocionFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.bumptech.glide.Glide
 import com.example.moodapp.R
+import kotlinx.android.synthetic.main.fragment_registra_emocion.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -20,12 +23,23 @@ private const val ARG_PARAM2 = "param2"
  */
 class RegistraEmocionFragment : Fragment() {
 
+    private lateinit var layoutFecha:ConstraintLayout
+    private lateinit var layoutHora:ConstraintLayout
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registra_emocion, container, false)
+        val view =inflater.inflate(R.layout.fragment_registra_emocion, container, false)
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Glide.with(view.context).load(R.drawable.ic_date).into(imagen_fecha)
+        Glide.with(view.context).load(R.drawable.ic_watch).into(imagen_hora)
     }
 
 
